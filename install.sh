e8089a54c9c733f7e47b22b9444f43ed099362a4
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2021-11-28 02:10:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/sh
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
   echo "Please enter dist name (eg. deb, void, arch)" && exit 1
-elif [ "$1" = "deb" ]
-then
-  sudo apt -y install fonts-jetbrains-mono xclip neovim ninja-build build-essentials cmake ccls
-elif [ "$1" = "void" ]
-then
-  sudo xbps-install -S -y xclip vis neovim ninja cmake base-devel zoxide xst dmenu ccls
-elif [ "$1" = "arch" ]
-then
-  sudo pacman -S --noconfirm ttf-jetbrains-mono xclip neovim vis ninja cmake base-devel zoxide dmenu premake meson ccls
+elif [ "$1" = "deb" ]; then
+  sudo apt -y install xclip neovim ninja-build build-essentials cmake ccls
+elif [ "$1" = "void" ]; then
+  sudo xbps-install -S -y xclip vis neovim ninja cmake base-devel xst autojump dmenu ccls
+elif [ "$1" = "arch" ]; then
+  sudo pacman -S --noconfirm xclip neovim vis ninja cmake base-devel dmenu premake meson ccls
+  git clone https://aur.archlinux.org/xst-git && cd xst-git/ && makepkg -si
+  git clone https://aur.archlinux.org/autojump && cd autojump/ && makepkg -si
 fi
 
 cp -f .profile .Xresources .bash_profile .bashrc .vimrc .xinitrc ~/
-
 chmod +x vpaste.sh && sudo cp -f vpaste.sh /usr/bin/vpaste.sh
 
-if [ -x "$(command -v nvim)" ]
-then
+if [ -x "$(command -v nvim)" ]; then
   sudo cp -f ./moonfly.vim /usr/share/nvim/runtime/colors/moonfly.vim
   mkdir -p ~/.config/nvim/pack/site/start
   cp ./init.vim ~/.config/nvim/init.vim
@@ -31,8 +27,7 @@ then
 
 fi
 
-if [ -x "$(command -v vis)" ]
-then
+if [ -x "$(command -v vis)" ]; then
   mkdir -p ~/.config/vis/themes
   cp ./visrc.lua ~/.config/vis/visrc.lua
   cp ./seti.lua ~/.config/vis/themes/seti.lua
