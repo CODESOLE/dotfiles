e0285aa3086487bfdfc7d015a714161de6a3d8a3
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2021-11-29 15:02:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 if [ -z "$1" ]; then
   echo "Please enter dist name (eg. deb, void, arch)" && exit 1
 elif [ "$1" = "deb" ]; then
-  sudo apt -y install xclip neovim ninja-build build-essentials cmake ccls
+  sudo apt -y install xclip neovim ninja-build build-essentials cmake ccls wget
 elif [ "$1" = "void" ]; then
-  sudo xbps-install -S -y xclip vis neovim ninja cmake base-devel xst autojump dmenu ccls xorg xinit
+  sudo xbps-install -S -y xclip vis neovim ninja cmake base-devel xst autojump dmenu ccls xorg xinit wget
   git clone https://git.suckless.org/dwm && && rm -f ./dwm/config.def.h && cp -f ./config.def.h ./dwm/config.def.h && cd ./dwm/ && sudo make clean install && cd ..
   git clone https://git.suckless.org/slstatus && rm -f ./slstatus/config.def.h && cp -f ./config.h ./slstatus/config.def.h && cd ./slstatus/ && sudo make clean install
 elif [ "$1" = "arch" ]; then
-  sudo pacman -S --noconfirm xclip neovim vis ninja cmake base-devel dmenu premake meson ccls xorg xorg-xinit
+  sudo pacman -S --noconfirm xclip neovim vis ninja cmake base-devel dmenu premake meson ccls xorg xorg-xinit wget
   git clone https://aur.archlinux.org/xst-git && cd xst-git/ && makepkg -si && cd ..
   git clone https://aur.archlinux.org/autojump && cd autojump/ && makepkg -si && cd ..
   git clone https://git.suckless.org/dwm && && rm -f ./dwm/config.def.h && cp -f ./config.def.h ./dwm/config.def.h && cd ./dwm/ && sudo make clean install && cd ..
@@ -23,6 +23,7 @@ sudo fc-cache -f -v
 chmod +x vpaste.sh && sudo cp -f vpaste.sh /usr/bin/vpaste.sh
 
 if [ -x "$(command -v nvim)" ]; then
+  wget -O- -q https://raw.githubusercontent.com/bluz71/vim-moonfly-colors/master/colors/moonfly.vim > moonfly.vim
   sudo cp -f ./moonfly.vim /usr/share/nvim/runtime/colors/moonfly.vim
   mkdir -p ~/.config/nvim/pack/site/start
   cp ./init.vim ~/.config/nvim/init.vim
