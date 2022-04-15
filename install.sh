4e6c712dd1cedbe1f36c14f0b3ed37b3d222b565
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-04-15 22:56:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,8 @@ if [ -x "$(command -v nvim)" ]; then
   git clone https://github.com/neovim/nvim-lspconfig &&\
   git clone https://github.com/nvim-treesitter/nvim-treesitter &&\
   git clone https://github.com/easymotion/vim-easymotion.git &&\
-  git clone https://github.com/mg979/vim-visual-multi.git
+  git clone https://github.com/mg979/vim-visual-multi.git &&\
+  git clone https://github.com/cjuniet/clang-format.vim.git
 fi
 
 if [ -x "$(command -v vis)" ]; then
