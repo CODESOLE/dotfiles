c527019733bfce2a2e616318c2f252b85f63c675
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-04-17 21:02:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp -f .profile .Xresources .bash_profile .bashrc .vimrc .xinitrc ~/
 chmod +x vpaste.sh && sudo cp -f vpaste.sh /usr/bin/vpaste.sh
 
 if [ -z "$1" ]; then
-  echo "Please enter dist name (eg. deb, void, arch)" && exit 1
+  echo "Please enter run type (eg. deb, void, arch, vis, nvim)" && exit 1
 elif [ "$1" = "deb" ]; then
   sudo apt -y install xclip neovim ninja-build build-essentials cmake ccls wget
 elif [ "$1" = "void" ]; then
@@ -17,11 +17,7 @@ elif [ "$1" = "arch" ]; then
   git clone https://aur.archlinux.org/autojump && cd autojump/ && makepkg -si && cd ..
   git clone https://git.suckless.org/dwm && rm -f ./dwm/config.def.h && cp -f ./config.def.h ./dwm/config.def.h && cd ./dwm/ && sudo make clean install && cd ..
   git clone https://git.suckless.org/slstatus && rm -f ./slstatus/config.def.h && cp -f ./config.h ./slstatus/config.def.h && cd ./slstatus/ && sudo make clean install
-fi
-
-if [ -x "$(command -v nvim)" ]; then
-  wget -O- -q https://raw.githubusercontent.com/bluz71/vim-moonfly-colors/master/colors/moonfly.vim > moonfly.vim
-  sudo cp -f ./moonfly.vim /usr/share/nvim/runtime/colors/moonfly.vim
+elif [ "$1" = "nvim" ] && [ -x "$(command -v nvim)" ]; then
   mkdir -p ~/.config/nvim/pack/site/start
   cp ./init.vim ~/.config/nvim/init.vim
   cd ~/.config/nvim/pack/site/start &&\
@@ -33,9 +29,7 @@ if [ -x "$(command -v nvim)" ]; then
   git clone https://github.com/airblade/vim-gitgutter &&\
   git clone https://github.com/kyazdani42/nvim-tree.lua &&\
   git clone https://github.com/kyazdani42/nvim-web-devicons
-fi
-
-if [ -x "$(command -v vis)" ]; then
+elif [ "$1" = "vis" ] && [ -x "$(command -v vis)" ]; then
   mkdir -p ~/.config/vis/themes
   cp ./visrc.lua ~/.config/vis/visrc.lua
   cp ./seti.lua ~/.config/vis/themes/seti.lua
