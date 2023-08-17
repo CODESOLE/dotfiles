7c651f7e0ab611672c74a5f4e7d13ff653ebc1d0
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2023-08-17 15:15:22 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -109,6 +109,7 @@ vim.keymap.set('i', '<C-k>', '<Up>')
 vim.keymap.set('i', '<C-j>', '<Down>')
 vim.keymap.set('i', '<C-h>', '<Left>')
 vim.keymap.set('i', '<C-l>', '<Right>')
+vim.keymap.set('n', '<C-s>', '<Cmd>SessionSave<CR>')
 
 vim.keymap.set('c', '<C-j>', '<Down>')
 vim.keymap.set('c', '<C-k>', '<Up>')
