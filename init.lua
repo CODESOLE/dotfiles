27af7514c937ef4d183226367f6c609348176d1c
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2023-10-26 14:55:28 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -205,12 +205,13 @@ require 'overseer'.patch_dap(true)
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+vim.keymap.set('n', '<leader>D', function() require('dapui').close() end, { noremap = true, silent = true })
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
 
 vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
 vim.keymap.set('n', '<leader>dj', function() require('dap').step_over() end)
