ba130ebaf2bca88cce15dc79b2db2e2e5eeb9e67
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2024-05-08 01:36:33 +0300

    Update init_BLOATED.lua

--- init_BLOATED.lua
+++ init_BLOATED.lua
@@ -265,13 +265,15 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fF', '<cmd>Telescope current_buffer_fuzzy_find<CR>', { noremap = false, silent = true })
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fw', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
 
 require 'neogit'.setup { integrations = { diffview = true }, use_telescope = true }
 vim.api.nvim_create_augroup("neogit-additions", {})
