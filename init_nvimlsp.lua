7c6b84f0898bf0be679f6a30719e319880e190bd
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2024-05-09 14:32:30 +0300

    Update init_nvimlsp.lua

--- init_nvimlsp.lua
+++ init_nvimlsp.lua
@@ -21,6 +21,7 @@ end
 
 bootstrap_paq {
   "savq/paq-nvim",
+  "puremourning/vimspector",
   "NeogitOrg/neogit",
   "sindrets/diffview.nvim",
   "nvim-tree/nvim-web-devicons",
@@ -32,21 +33,10 @@ bootstrap_paq {
   "echasnovski/mini.jump2d",
   "nvim-lua/plenary.nvim",
   "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "mfussenegger/nvim-dap",
-  "theHamsta/nvim-dap-virtual-text",
-  "jay-babu/mason-nvim-dap.nvim",
-  "rcarriga/nvim-dap-ui",
-  "nvim-neotest/nvim-nio",
-  "stevearc/overseer.nvim",
   "stevearc/oil.nvim",
   "j-hui/fidget.nvim",
   "nvim-lualine/lualine.nvim",
-  "Bekaboo/dropbar.nvim",
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-telescope/telescope.nvim" , branch = "0.1.6"},
-  { "nvim-telescope/telescope-fzf-native.nvim" , build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"},
 }
 vim.cmd("set termguicolors")
 vim.g.moonflyWinSeparator = 2
@@ -69,28 +59,20 @@ vim.cmd('au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch
 vim.cmd('colorscheme moonfly')
 require('mini.surround').setup()
 require('mini.jump2d').setup()
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "rust", "zig", "go" },
-  auto_install = false,
-  highlight = { enable = true },
-}
 require('fidget').setup()
 require('lualine').setup { sections = {
   lualine_a = { 'branch' },
-  lualine_b = { 'filename' },
+  lualine_b = { { 'filename', path = 1 } },
   lualine_c = { 'diff', '' },
   lualine_x = { '', '', 'diagnostics' },
   lualine_y = { 'location' },
   lualine_z = { 'searchcount' },
 } }
 require('neogit').setup{}
-require('oil').setup{view_options = {show_hidden = true }}
+require('oil').setup()
 require('mini.pairs').setup()
 require('mini.completion').setup()
 require('diffview').setup()
-require('mason').setup()
-require('mason-nvim-dap').setup()
-require('mason-lspconfig').setup()
 require('lspconfig').clangd.setup{}
 require('lspconfig').rust_analyzer.setup{}
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -103,46 +85,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
   end,
 })
-require('overseer').setup{ dap = false }
-require 'dap.ext.vscode'.json_decode = require "overseer.json".decode
-require 'dap.ext.vscode'.load_launchjs(nil, { codelldb = { 'c', 'cpp', 'rust' } })
-require 'overseer'.patch_dap(true)
-require("dapui").setup()
-local dap, dapui = require("dap"), require("dapui")
-require("nvim-dap-virtual-text").setup()
-dap.adapters.codelldb = {
-  type = 'server',
-  port = "13000",
-  executable = {
-    command = vim.fn.stdpath('data') .. '/mason/bin/codelldb',
-    args = { "--port", "13000" },
-    detached = false,
-  }
-}
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-vim.keymap.set('n', '<leader>D', function() require('dapui').close() end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
-vim.keymap.set('n', '<leader>dj', function() require('dap').step_over() end)
-vim.keymap.set('n', '<leader>dk', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp',
-  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function() require('dap.ui.widgets').hover() end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function() require('dap.ui.widgets').preview() end)
-vim.keymap.set('n', '<Leader>dd', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<Leader>ds', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.scopes)
-end)
 require('telescope').setup {
   pickers = {
     buffers = {
@@ -153,7 +95,6 @@ require('telescope').setup {
     }
   }
 }
-require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
@@ -197,3 +138,17 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-N>')
 vim.keymap.set('n', '<leader>l', ':cn<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>h', ':cp<cr>', { noremap = true, silent = true })
 vim.keymap.set("n", "q", "<nop>", {})
+vim.cmd 'nnoremap <Leader>dr <Plug>VimspectorRestart'
+vim.cmd 'nnoremap <Leader>de <Plug>VimspectorStop'
+vim.cmd 'nnoremap <Leader>dc <Plug>VimspectorContinue'
+vim.cmd 'nnoremap <Leader>dt <Plug>VimspectorRunToCursor'
+vim.cmd 'nnoremap <Leader>db :call vimspector#ToggleBreakpoint()<CR>'
+vim.cmd 'nnoremap <Leader>dl <Plug>VimspectorToggleConditionalBreakpoint'
+vim.cmd 'nnoremap <Leader><Up> <Plug>VimspectorUpFrame'
+vim.cmd 'nnoremap <Leader><Down> <Plug>VimspectorDownFrame'
+vim.cmd 'nnoremap <Leader>dh <Plug>VimspectorStepOut'
+vim.cmd 'nnoremap <Leader>dk <Plug>VimspectorStepInto'
+vim.cmd 'nnoremap <Leader>dj <Plug>VimspectorStepOver'
+vim.cmd 'nnoremap <Leader>dp <Plug>VimspectorBalloonEval'
+vim.cmd 'vnoremap <Leader>dp <Plug>VimspectorBalloonEval'
+vim.cmd 'nnoremap <Leader>B <Plug>VimspectorBreakpoints'
