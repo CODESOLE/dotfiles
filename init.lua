529d6d9095d7bc3b9b9326554fd9671ee1e154e5
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2024-07-15 16:27:55 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -227,6 +227,28 @@ dap.adapters.lldb = {
   command = 'C:/Program Files/LLVM/bin/lldb-dap.exe',
   name = 'lldb'
 }
+dap.configurations.rust = {
+  {
+    initCommands = function()
+          local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+          local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+          local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+          local commands = {}
+          local file = io.open(commands_file, 'r')
+          if file then
+            for line in file:lines() do
+              table.insert(commands, line)
+            end
+            file:close()
+          end
+          table.insert(commands, 1, script_import)
+
+          return commands
+    end,
+  },
+}
 dap.listeners.before.attach.dapui_config = function()
   dapui.open()
   vim.cmd'set signcolumn=yes'
