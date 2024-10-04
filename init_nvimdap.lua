7de2e90647950954520ed236c09caca1dd6e1ead
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2024-10-05 00:16:40 +0300

    Update init_nvimdap.lua

--- init_nvimdap.lua
+++ init_nvimdap.lua
@@ -84,7 +84,7 @@ require'nvim-treesitter.configs'.setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      init_selection = "gN", -- set to `false` to disable one of the mappings
       node_incremental = "grn",
       scope_incremental = "grc",
       node_decremental = "grm",
