34342b5b2c04cfd2577a595e7aaaa0589b1e94e7
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2025-01-27 13:05:39 +0300

    Create init_nvim_fzflua.lua

---
+++ init_nvim_fzflua.lua
@@ -0,0 +1,484 @@
+vim.cmd('lang en_US.UTF-8')
+vim.keymap.set("v", "ş", "<Esc>")
+vim.keymap.set("c", "ş", "<Esc>")
+vim.keymap.set("i", "ş", "<Esc>")
+local function clone_paq()
+  local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+  local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
+  if not is_installed then
+    vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+    return true
+  end
+end
+local function bootstrap_paq(packages)
+  local first_install = clone_paq()
+  vim.cmd.packadd("paq-nvim")
+  local paq = require("paq")
+  if first_install then
+    vim.notify("Installing plugins... If prompted, hit Enter to continue.")
+  end
+  paq(packages)
+  paq.install()
+end
+bootstrap_paq {
+  "savq/paq-nvim",
+  "TaDaa/vimade",
+  "bluz71/vim-moonfly-colors",
+  "j-hui/fidget.nvim",
+  "nmac427/guess-indent.nvim",
+  "NeogitOrg/neogit",
+  "NStefan002/visual-surround.nvim",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+  "akinsho/toggleterm.nvim",
+  "echasnovski/mini.pairs",
+  "echasnovski/mini.files",
+  "ggandor/leap.nvim",
+  "neovim/nvim-lspconfig",
+  "mfussenegger/nvim-dap",
+  "nvim-neotest/nvim-nio",
+  "rcarriga/nvim-dap-ui",
+  "theHamsta/nvim-dap-virtual-text",
+  "nvim-lualine/lualine.nvim",
+  "nvim-lua/plenary.nvim",
+  "sindrets/diffview.nvim",
+  "ibhagwan/fzf-lua",
+  "nvim-treesitter/nvim-treesitter-context",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  { "nvim-treesitter/nvim-treesitter", build = ':TSUpdate' }
+}
+vim.g.moonflyWinSeparator = 2
+vim.g.moonflyVirtualTextColor = true
+vim.o.updatetime = 200
+vim.cmd.colorscheme('moonfly')
+vim.wo.wrap = false
+vim.wo.list = true
+vim.wo.listchars = "tab:> ,trail:-,nbsp:+,extends:>,precedes:<"
+vim.go.fillchars = "eob: "
+vim.api.nvim_set_hl(0, 'NonText', {bg='#999999', fg='black'})
+vim.api.nvim_set_hl(0, 'EndOfBuffer', {bg='NONE'})
+vim.o.showmode     = false
+vim.o.swapfile     = false
+vim.o.backup     = false
+vim.o.writebackup = false
+vim.o.smartcase    = true
+vim.o.ignorecase   = true
+vim.o.smartindent  = true
+vim.o.autoindent   = true
+vim.o.showmatch    = true
+vim.o.cmdheight    = 0
+vim.o.signcolumn   = "no"
+vim.g.mapleader    = ' '
+vim.keymap.set("v", "<leader>y", '"*y', { silent = true })
+vim.cmd('au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=500}')
+vim.keymap.set("v", "<leader>g", require'neogit'.open, { silent = true, noremap = true })
+require('fzf-lua').setup{'fzf-native'}
+require('neogit').setup{}
+vim.keymap.set("n", "<leader>g", require'neogit'.open, { silent = true, noremap = true })
+require'toggleterm'.setup{ open_mapping = [[<C-j>]], shell = 'nu.exe' }
+vim.keymap.set("n", "<C-p>", ":ToggleTerm direction=float<CR>", { silent = true, noremap = true })
+require('guess-indent').setup {}
+require'fidget'.setup()
+require("diffview").setup({use_icons=false})
+require'mini.files'.setup{ windows = { preview = true } }
+vim.keymap.set("n", "<leader>o", "<CMD>lua MiniFiles.open()<CR>", { silent = true })
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "go", "c", "cpp", "rust", "toml", "zig", "lua", "query" },
+  sync_install = false,
+  auto_install = true,
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gN", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  textobjects = {
+    swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+    },
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      include_surrounding_whitespace = true,
+    },
+    lsp_interop = {
+      enable = true,
+      floating_preview_opts = { border = 'none' },
+      peek_definition_code = {
+        ["<leader>p"] = "@function.outer",
+        ["<leader>P"] = "@class.outer",
+      },
+    },
+  },
+}
+vim.wo.foldmethod = 'expr'
+vim.o.foldenable = false
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+require('lualine').setup { options = { icons_enabled = false, section_separators = '', component_separators = '' }, sections = {
+  lualine_a = { 'branch' },
+  lualine_b = { 'fileformat', 'encoding', 'filetype' },
+  lualine_c = { 'filesize' },
+  lualine_x = { '%L', 'selectioncount', 'progress' },
+  lualine_y = { 'location' },
+  lualine_z = { 'searchcount' },
+}, inactive_sections = {
+  lualine_a = { 'branch' },
+  lualine_b = { 'fileformat', 'encoding', 'filetype' },
+  lualine_c = { 'filesize' },
+  lualine_x = { '%L', 'selectioncount', 'progress' },
+  lualine_y = { 'location' },
+  lualine_z = { 'searchcount' },
+}, winbar = {
+  lualine_b = { { 'filename', path=1 } },
+  lualine_c = { 'diagnostics' },
+  lualine_y = { 'diff' },
+}, inactive_winbar = {
+  lualine_b = { { 'filename', path=1 } },
+  lualine_c = { 'diagnostics' },
+  lualine_y = { 'diff' },
+} }
+vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap)')
+vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
+require('mini.pairs').setup()
+require('visual-surround').setup{enable_wrapped_deletion = true}
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+cmp.setup({
+   snippet = {
+     -- REQUIRED - you must specify a snippet engine
+     expand = function(args)
+       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+     end,
+   },
+   window = {
+     completion = cmp.config.window.bordered(),
+     documentation = cmp.config.window.bordered(),
+   },
+   mapping = cmp.mapping.preset.insert({
+     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+     ['<C-Space>'] = cmp.mapping.complete(),
+     ['<C-e>'] = cmp.mapping.abort(),
+     ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+   }),
+   sources = cmp.config.sources({
+     { name = 'nvim_lsp' },
+     { name = 'luasnip' }, -- For luasnip users.
+   }, {
+     { name = 'buffer' },
+   })
+ })
+ -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+ cmp.setup.cmdline({ '/', '?' }, {
+   mapping = cmp.mapping.preset.cmdline(),
+   sources = {
+     { name = 'buffer' }
+   }
+ })
+
+ -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+ cmp.setup.cmdline(':', {
+   mapping = cmp.mapping.preset.cmdline(),
+   sources = cmp.config.sources({
+     { name = 'path' }
+   }, {
+     { name = 'cmdline' }
+   }),
+   matching = { disallow_symbol_nonprefix_matching = false }
+ })
+ -- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').gopls.setup{
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      }
+    }
+  }
+}
+require('lspconfig').clangd.setup{capabilities = capabilities,}
+require('lspconfig').zls.setup{capabilities = capabilities,}
+require('lspconfig').rust_analyzer.setup{capabilities = capabilities,}
+vim.filetype.add{ extension = { v = 'vlang' } }
+local lspconfig = require('lspconfig')
+local util = require('lspconfig/util')
+require('lspconfig').v_analyzer.setup{ filetypes = { 'vlang' }, capabilities = capabilities, }
+require('lspconfig').v_analyzer.setup{ filetypes = { 'vlang' }, capabilities = capabilities, }
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fl", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').git_files()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fb", "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fs", "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fw", "<cmd>lua require('fzf-lua').lsp_live_workspace_symbols()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fr", "<cmd>lua require('fzf-lua').lsp_references()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fa", "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fd", "<cmd>lua require('fzf-lua').diagnostics_document()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fD", "<cmd>lua require('fzf-lua').diagnostics_workspace()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fo", "<cmd>lua require('fzf-lua').oldfiles()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fc", "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
+vim.lsp.inlay_hint.enable(true)
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<space>fa', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set({'n', 'v'}, '<space>F', vim.lsp.buf.format, opts)
+    if vim.lsp.get_client_by_id(ev.data.client_id).server_capabilities.documentHighlightProvider then
+          vim.cmd [[
+            hi! LspReferenceRead cterm=bold ctermbg=Gray guibg=#323437
+            hi! LspReferenceText cterm=bold ctermbg=Gray guibg=#323437
+            hi! LspReferenceWrite cterm=bold ctermbg=Gray guibg=#323437
+          ]]
+          vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
+          vim.api.nvim_clear_autocmds({
+            buffer = ev.buf,
+            group = 'lsp_document_highlight',
+          })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            group = 'lsp_document_highlight',
+            buffer = ev.buf,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            group = 'lsp_document_highlight',
+            buffer = ev.buf,
+            callback = vim.lsp.buf.clear_references,
+          })
+    end
+  end,
+})
+vim.keymap.set('n', 'gl', '$')
+vim.keymap.set('n', 'gh', '0')
+vim.keymap.set('n', 'gn', ':bn<CR>')
+vim.keymap.set('n', 'gp', ':bp<CR>')
+vim.keymap.set('i', '<C-k>', '<Up>')
+vim.keymap.set('i', '<C-j>', '<Down>')
+vim.keymap.set('i', '<C-h>', '<Left>')
+vim.keymap.set('i', '<C-l>', '<Right>')
+vim.keymap.set('c', '<C-j>', '<Down>')
+vim.keymap.set('c', '<C-k>', '<Up>')
+vim.keymap.set('c', '<C-h>', '<Left>')
+vim.keymap.set('c', '<C-l>', '<Right>')
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
+vim.keymap.set('i', '<A-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('i', '<A-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('i', '<A-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('i', '<A-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('t', '<A-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('t', '<A-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('t', '<A-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('t', '<A-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('t', '<A-m>', '<C-\\><C-N>')
+vim.keymap.set('n', '<leader>l', ':cn<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>h', ':cp<cr>', { noremap = true, silent = true })
+vim.keymap.set("n", "q", "<nop>", {})
+require("dapui").setup()
+local dap, dapui = require("dap"), require("dapui")
+require("nvim-dap-virtual-text").setup()
+dap.adapters.lldb = {
+  type = 'executable',
+  command = 'C:/Program Files/LLVM/bin/lldb-dap.exe',
+  name = 'lldb'
+}
+dap.configurations.cpp = {
+  {
+    name = '[LLDB] Launch Manually',
+    type = 'lldb',
+    request = 'launch',
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    program = function()
+      local pickers = require("telescope.pickers")
+      local finders = require("telescope.finders")
+      local conf = require("telescope.config").values
+      local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
+      return coroutine.create(function(coro)
+        local opts = {}
+        pickers
+          .new(opts, {
+            prompt_title = "Path to executable",
+            finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x" }, {}),
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(buffer_number)
+              actions.select_default:replace(function()
+                actions.close(buffer_number)
+                coroutine.resume(coro, action_state.get_selected_entry()[1])
+              end)
+              return true
+            end,
+          })
+          :find()
+      end)
+    end,
+    args = function()
+      local args = vim.fn.input('Args: ', '', 'file')
+      local words = {}
+      for v in args:gmatch('%S+') do
+        table.insert(words, v)
+      end
+      if #words == 0 then
+        return {}
+      end
+      return words
+    end,
+    env = function()
+      local variables = {}
+      for k, v in pairs(vim.fn.environ()) do
+        table.insert(variables, string.format("%s=%s", k, v))
+      end
+      table.insert(variables, function() return vim.fn.input('ENV var: ', '', 'file') end)
+      return variables
+    end,
+  },
+  {
+    name = '[LLDB] Attach to Process',
+    type = 'lldb',
+    request = 'attach',
+    pid = require('dap.utils').pick_process,
+    args = {},
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.zig = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+dap.configurations.rust[1].initCommands = function()
+  if jit.os == "Linux" or jit.os == "BSD" or jit.os == "OSX" or jit.os == "POSIX" then
+    local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+    local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+    local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+    local commands = {}
+    local file = io.open(commands_file, 'r')
+    if file then
+      for line in file:lines() do
+        table.insert(commands, line)
+      end
+      file:close()
+    end
+    table.insert(commands, 1, script_import)
+    return commands
+  end
+end
+dap.configurations.rust[2].initCommands = dap.configurations.rust[1].initCommands
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+  vim.cmd'set signcolumn=yes'
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+  vim.cmd'set signcolumn=yes'
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+  vim.cmd'set signcolumn=no'
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+  vim.cmd'set signcolumn=no'
+end
+vim.keymap.set('n', '<leader>D', function() require('dapui').close() vim.cmd'set signcolumn=no' end, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
+vim.keymap.set('n', '<leader>dj', function() require('dap').step_over() end)
+vim.keymap.set('n', '<leader>dk', function() require('dap').step_into() end)
+vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>de', ':DapTerminate<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>b', ':FzfLua dap_breakpoints<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>c', ':FzfLua dap_configurations<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>dt', function() require('dap').run_to_cursor() end)
+vim.keymap.set('n', '<Leader><Up>', function() require('dap').up() end)
+vim.keymap.set('n', '<Leader><Down>', function() require('dap').down() end)
+vim.keymap.set('n', '<Leader>L',
+  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function() require('dap.ui.widgets').hover() end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function() require('dap.ui.widgets').preview() end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
