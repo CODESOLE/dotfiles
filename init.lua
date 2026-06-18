25190bad621c7d921da63a1aaf12499b5b15a912
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2026-06-18 20:50:21 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.keymap.set('c', '<C-l>', '<Right>')
 vim.keymap.set('n', '<leader>l', ':cn<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>h', ':cp<cr>', { noremap = true, silent = true })
 vim.keymap.set("n", "q", "<nop>", {})
-vim.keymap.set("n", "<C-t>", ":vs | term<CR>", {noremap = true})
 vim.keymap.set({'n', 'v', 'i', 'c' }, 'ş', '<Esc>')
 vim.keymap.set('t', 'ş', '<C-\\><C-N>')
 vim.wo.wrap = false
@@ -32,9 +31,8 @@ vim.o.cmdheight    = 0
 vim.o.signcolumn   = "no"
 vim.g.mapleader    = ' '
 vim.diagnostic.config({ virtual_text = true })
-vim.cmd('au TextYankPost * silent! lua vim.hl.on_yank {higroup="IncSearch", timeout=500}')
+-- vim.cmd('au TextYankPost * silent! lua vim.hl.on_yank {higroup="IncSearch", timeout=500}')
 vim.g.vimtex_view_method = "zathura"
-vim.cmd.colorscheme("vim")
 vim.o.autocomplete       = true
 vim.o.autocompletedelay  = 200
 vim.o.complete = ".,w,b,o"
@@ -44,35 +42,14 @@ vim.o.pummaxwidth = 80
 vim.opt.shortmess:append("c")
 vim.pack.add({
   "https://github.com/lervag/vimtex",
-  "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/echasnovski/mini.pairs",
   "https://github.com/echasnovski/mini.files",
-  "https://codeberg.org/andyg/leap.nvim",
   "https://github.com/ibhagwan/fzf-lua",
 })
-require('lualine').setup { options = { icons_enabled = false, section_separators = '', component_separators = '' }, sections = {
-  lualine_a = { 'branch' },
-  lualine_b = { 'fileformat', 'encoding', 'filetype' },
-  lualine_c = { 'filesize', 'filename', 'diff' },
-  lualine_x = { 'diagnostics', '%L', 'selectioncount', 'progress' },
-  lualine_y = { 'location' },
-  lualine_z = { 'searchcount' },
-}, inactive_sections = {
-  lualine_a = { 'branch' },
-  lualine_b = { 'fileformat', 'encoding', 'filetype' },
-  lualine_c = { 'filesize', 'filename', 'diff' },
-  lualine_x = { 'diagnostics', '%L', 'selectioncount', 'progress' },
-  lualine_y = { 'location' },
-  lualine_z = { 'searchcount' },
-} }
 vim.keymap.set('n', '<leader><leader>', ':FzfLua global<CR>', {silent = true, noremap = true})
 vim.keymap.set('n', '<leader>g',  ':FzfLua live_grep<CR>', {silent = true, noremap = true})
-require('mini.pairs').setup()
 require'mini.files'.setup{ windows = { preview = true } }
 vim.keymap.set("n", "<leader>o", "<CMD>lua MiniFiles.open()<CR>", { silent = true })
-vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap)')
-vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
