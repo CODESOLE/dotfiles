2471fae4d9fe82e6de490b8268c16e40aaffb1a2
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2026-08-10 15:21:57 +0300

    Change keymap for build_and_run to Ctrl+V

--- init.lua
+++ init.lua
@@ -87,5 +87,5 @@ local function build_and_run()
     end
   end, { buffer = buf, silent = true, desc = "Close output window" })
 end
-vim.keymap.set("n", "<leader>r", build_and_run, { desc = "Build and run in floating window" })
+vim.keymap.set("n", "<C-v>", build_and_run, { desc = "Build and run in floating window" })
 vim.keymap.set("n", "<C-c>", "<cmd>mak<CR>", { desc = "Build" })
